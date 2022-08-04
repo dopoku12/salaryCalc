6a327f2d0161e5847c6e0bbfa0422e426ff10a7e
David Opoku <71300793+dopoku12@users.noreply.github.com>
2022-08-04 17:06:54 -0400

    li ids made

--- src/Form.js
+++ src/Form.js
@@ -4,64 +4,67 @@ function submit() {
 }
 
 const Form = () => {
+
+
+    const inputFields = [
+        {
+            id: 0,
+            label: 'Annual Pay:',
+            placeholder: 'Enter Annual Pay',
+        },
+        {
+            id: 1,
+            label: ' Bi-Weekly Pay:',
+            placeholder: 'Enter Bi-Weekly Pay',
+        },
+        {
+            id: 2,
+            label: 'Weekly Pay:',
+            placeholder: 'Enter Weekly Pay',
+        },
+        {
+            id: 3,
+            label: 'Daily Pay:',
+            placeholder: 'Enter Daily Pay',
+        },
+        {
+            id: 4,
+            label: 'Hourly Pay:',
+            placeholder: 'Enter Hourly Pay',
+        },
+
+    ]
+
+
+
+
+
+
     const [usrInput, setUsrInput] = useState()
 
     return (
         <form onSubmit={submit}>
+            {
+                inputFields.map((i) => {
+                    return (
+                        <li id={i.id}>
+                            <label>
+                                {i.label}
+                            </label>
 
-            <label>
-                Yearly Pay:
-            </label>
-            <br />
-            <input type="number" placeholder="Enter Yearly Pay"
-                value={usrInput}
-                onchange={(e) => { setUsrInput(e.target.value) }}
-            />
-            <br />
-
-            <label>
-                Bi-Weekly Pay:
-            </label>
-            <br />
-            <input type="number" placeholder="Enter Bi-weekly Pay"
-                value={usrInput}
-                onChange={(e) => { setUsrInput(e.target.value) }}
-            />
-            <br />
-
-
-            <label>
-                Weekly Pay:
-            </label>
-
-            <br />
-            <input type="number" placeholder="Enter Weekly Pay"
-                value={usrInput}
-                onChange={(e) => { setUsrInput(e.target.value) }}
-            />
-            <br />
-
-            <label >
-                Daily Pay:
-            </label>
-
-            <br />
-            <input type="number" placeholder="Enter Daily Pay"
-                value={usrInput}
-                onChange={(e) => { setUsrInput(e.target.value) }}
-            />
-            <br />
-
-            <label >
-                Hourly Pay:
-            </label>
-
-            <br />
-            <input type="number" placeholder="Enter Hourly Pay"
-                value={usrInput}
-                onChange={(e) => { setUsrInput(e.target.value) }}
-            />
-            <br />
+                            <br />
+                            <input type="number" placeholder={i.placeholder}
+                                value={usrInput}
+                                onchange={(e) => {
+                                    setUsrInput(e.target.value)
+                                }}
+                            />
+                            <br />
+                        </li>
+                    )
+                }
+                )
+            }
 
 
             <button>
