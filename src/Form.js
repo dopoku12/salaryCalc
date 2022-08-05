9b96b8389bddc01f4bb8b501837ae3fd5b7a4608
David Opoku <71300793+dopoku12@users.noreply.github.com>
2022-08-04 23:27:39 -0400

    need formula for value

--- src/Form.js
+++ src/Form.js
@@ -1,65 +1,73 @@
 import { useState } from "react";
-function submit() {
-
-}
-
 const Form = () => {
-
-
+    const [usrInput, setUsrInput] = useState('')
     const inputFields = [
         {
             id: 0,
             label: 'Annual Pay:',
             placeholder: 'Enter Annual Pay',
+            formula: 40
         },
+        {
+            id: 3,
+            label: 'Monthly Pay:',
+            placeholder: 'Enter Monthly Pay',
+            formula: 40
+        },
+
+
         {
             id: 1,
             label: ' Bi-Weekly Pay:',
             placeholder: 'Enter Bi-Weekly Pay',
+            formula: 40
         },
         {
             id: 2,
             label: 'Weekly Pay:',
             placeholder: 'Enter Weekly Pay',
+            formula: 40
         },
-        {
-            id: 3,
-            label: 'Daily Pay:',
-            placeholder: 'Enter Daily Pay',
-        },
+
         {
             id: 4,
             label: 'Hourly Pay:',
             placeholder: 'Enter Hourly Pay',
+            formula: 10
         },
-
     ]
 
 
 
+    function submit(e) {
+        e.preventDefault()
+        const num = parseInt(usrInput)
+        const Output = num
+        setUsrInput(Output)
+    }
 
-
-
-    const [usrInput, setUsrInput] = useState()
-
+    // if id dosen't === id on change then add formula
     return (
         <form onSubmit={submit}>
             {
                 inputFields.map((i) => {
                     return (
-                        <li id={i.id}>
+                        <li key={i.id}>
                             <label>
                                 {i.label}
                             </label>
 
                             <br />
                             <input type="number" placeholder={i.placeholder}
-                                value={usrInput}
-                                onchange={(e) => {
+                                value={
+                                    usrInput + i.formula
+                                }
+                                onChange={(e) => {
+
                                     setUsrInput(e.target.value)
                                 }}
                             />
-                            <br />
+                            <br /> <br />
                         </li>
                     )
                 }
@@ -71,6 +79,9 @@ const Form = () => {
                 submit
             </button>
 
+            <button>
+                Clear
+            </button>
         </form>
 
     );
